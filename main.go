7f1640c7cd9d56d500df26c9ce2401f14d5857a3
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-06-07 23:36:55 +0900

    Can setting port

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+	"flag"
 )
 
 const ROOT string = "/"
@@ -20,17 +21,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.template = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.template.Execute(w, nil)
+	t.template.Execute(w, r)
 }
 
 func main() {
+	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	
 	r := newRoom()
 	http.Handle(ROOT, &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	
 	go r.run()
 	
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+	
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
