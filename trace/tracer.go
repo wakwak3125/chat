853be8dc9037ab6e51af04fbc6cfa205940f6cef
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-06-11 23:17:06 +0900

    Add trace

---
+++ trace/tracer.go
@@ -0,0 +1,35 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+type Tracer interface {
+	Trace(...interface{})
+}
+
+type nilTracer struct {
+
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func (t *nilTracer) Trace(a ...interface{}) {
+	// no-op
+}
+
+func Off() Tracer {
+	return &nilTracer{}
+}
